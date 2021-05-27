3e03e2f4519a633270457414baef42a92483ca2f
Fabian Töpfer <fabian.toepfer@tu-dresden.de>
2021-05-27 10:03:05 +0200

    adding rough FFT and audio nodes

--- DEMO_tracker/src/DEMO_trackerApp.cpp
+++ DEMO_tracker/src/DEMO_trackerApp.cpp
@@ -1,6 +1,7 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/audio/audio.h"
 
 #include "OSCServer.h"
 
@@ -15,8 +16,14 @@ class DEMO_trackerApp : public App {
 	void update() override;
 	void draw() override;
 
+	void drawSpectrumPlot(const vector<float>& magSpectrum);
+
+
 //	std::shared_ptr<Sender> sender;
 	std::shared_ptr<OSCServer>			m_server;
+	audio::InputDeviceNodeRef			m_inputNode;
+	audio::MonitorSpectralNodeRef		m_monitorSpectralNode;
+	vector<float>						mMagSpectrum;
 
 
 };
@@ -24,6 +31,18 @@ class DEMO_trackerApp : public App {
 void DEMO_trackerApp::setup()
 {
 	m_server = std::make_shared<OSCServer>(3339);
+	
+	
+	
+	auto ctx = ci::audio::master();
+
+	m_inputNode = ctx->createInputDeviceNode();
+
+	m_monitorSpectralNode = ctx->makeNode(new audio::MonitorSpectralNode());
+
+	m_inputNode >> m_monitorSpectralNode;
+	m_inputNode->enable();
+	ctx->enable();
 }
 
 void DEMO_trackerApp::mouseDown( MouseEvent event )
@@ -39,11 +58,67 @@ void DEMO_trackerApp::mouseDown( MouseEvent event )
 
 void DEMO_trackerApp::update()
 {
+	mMagSpectrum = m_monitorSpectralNode->getMagSpectrum();
+	for (auto& i : mMagSpectrum) {
+		console() << i << std::endl;
+	}
 }
 
 void DEMO_trackerApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear();
+	gl::enableAlphaBlending();
+
+	drawSpectrumPlot(mMagSpectrum);
+
 }
+
+void DEMO_trackerApp::drawSpectrumPlot(const vector<float>& magSpectrum) 	
+{
+		if (magSpectrum.empty())
+			return;
+
+		gl::ScopedGlslProg glslScope(getStockShader(gl::ShaderDef().color()));
+
+		ColorA bottomColor(0, 0, 1, 1);
+
+		float width = getWindowWidth();
+		float height = getWindowHeight();
+		size_t numBins = magSpectrum.size();
+		float padding = 0;
+		float binWidth = (width - padding * (numBins - 1)) / (float)numBins;
+
+		gl::VertBatch batch(GL_TRIANGLE_STRIP);
+
+		size_t currVertex = 0;
+		float m;
+		Rectf bin(getWindowBounds().x1, getWindowBounds().y1, getWindowBounds().x1 + binWidth, getWindowBounds().y2);
+		for (size_t i = 0; i < numBins; i++) {
+			m = magSpectrum[i];
+			//if (mScaleDecibels)
+			m = audio::linearToDecibel(m) / 100;
+
+			bin.y1 = bin.y2 - m * height;
+
+			batch.color(bottomColor);
+			batch.vertex(bin.getLowerLeft());
+			batch.color(0, m, 0.7f);
+			batch.vertex(bin.getUpperLeft());
+
+			bin += vec2(binWidth + padding, 0);
+			currVertex += 2;
+		}
+
+		batch.color(bottomColor);
+		batch.vertex(bin.getLowerLeft());
+		batch.color(0, m, 0.7f);
+		batch.vertex(bin.getUpperLeft());
+
+		gl::color(0, 0.9f, 0);
+
+		batch.draw();
+
+	}
+
 
 CINDER_APP( DEMO_trackerApp, RendererGl )
